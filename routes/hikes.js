d283a99bd5574f67502fac1c720b45c1c43dcebc
ianmacdonald <ianpetercreative@gmail.com>
2023-11-01 10:08:42 -0600

    add ensureLoggedIn function

--- routes/hikes.js
+++ routes/hikes.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const hikesCtrl = require('../controllers/hikes')
+const hikesCtrl = require('../controllers/hikes');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // All routes prepended with /hikes 
 router.get('/', hikesCtrl.index);
 
-router.get('/new', hikesCtrl.new);
+router.get('/new', ensureLoggedIn, hikesCtrl.new);
 
 router.get('/:id', hikesCtrl.show)
 
-router.post('/', hikesCtrl.create)
+router.post('/', ensureLoggedIn, hikesCtrl.create)
 
 module.exports = router;
